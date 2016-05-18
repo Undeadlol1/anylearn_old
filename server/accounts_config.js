5da7e51553b80e6e3adbac40b764476392642249
Mihail Paley <undeadlol1@mail.ru>
2016-05-18 16:03:06 +0300

    improves text in template.js

--- server/accounts_config.js
+++ server/accounts_config.js
@@ -2,7 +2,7 @@ import { Accounts } from 'meteor/accounts-base'
 import { email, username } from './PROTECTED_VARIABLES'
 
 Accounts.onCreateUser(function(options, user) {
-  console.log('onCreateUser is fired!')
+  //console.log('onCreateUser is fired!')
   if (options.username === username || options.email === email) options.profile.isAdmin = true;
   if (options.profile) user.profile = options.profile
   return user;
