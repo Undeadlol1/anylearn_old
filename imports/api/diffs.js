41ab2bd92fe287909699fb0433a690e48a2c6f51
Mihail Paley <undeadlol1@mail.ru>
2016-06-14 00:23:35 +0300

    add diff collection template

--- imports/api/diffs.js
+++ imports/api/diffs.js
@@ -1,7 +1,10 @@
 import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 import { check } from 'meteor/check'
+const JsDiff = require('diff')
 
+/* THIS PART IS UNFINISHED */
+// perfomance optimisation is not an issue now so this is temporary abandoned
 export const Diffs = new Mongo.Collection('diffs')
 
 if (Meteor.isServer) {
@@ -13,25 +16,23 @@ if (Meteor.isServer) {
 
 Meteor.methods({
   'diffs.insert' (data) {
-    check(data.why, String)
-    check(data.exp, String)
-    check(data.parent, String)
+    //check(data.why, String)
+    //check(data.exp, String)
+    //check(data.parent, String)
 
     // Make sure the user is logged in before inserting
     if (!Meteor.userId()) {
       throw new Meteor.Error('not-authorized')
     }
     // do not allow multiple diffs
-    if (Diffs.findOne({parent: data.parent, userId: Meteor.userId()})) {
-      throw new Meteor.Error('response-already-exists')
-    }
-
-    return Diffs.insert({
-          why: data.why,
-          exp: data.exp,
-          parent: data.parent,
-          createdAt: new Date(),
-          userId: Meteor.userId()
-    })
+    const __html = JsDiff
+                  .diffTrimmedLines(this.props.first, this.props.second)
+                  .map(function(part, index){
+                        // added = 'green', removed = 'red', else = ''
+                        const color = part.added ? 'green' : part.removed ? 'red' : ''
+                        if (!color) return null // do not return untouched bits
+                        return `<div key=${index} style="background-color: ${color};">${part.value}</div>`
+                  })
+                  .join('')
   }
 })
